751b292964ab8eeabb8183f4c34b6218cf87d3a2
11September <shiptya@icloud.com>
2019-03-15 14:22:33 +0200

    PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    $posts = Post::where('status', 'PUBLISHED')->latest()->get();
+
+    return view('blog', compact('posts'));
 }
